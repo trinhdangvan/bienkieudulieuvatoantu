d6be0e8a45864f863dc228daeb3aca3debec31a1
trinhdangvan <tuehue03@gmail,com>
2020-10-07 15:52:52 +0700

    hoan thanh

---
+++ baitap1.js
@@ -0,0 +1,9 @@
+let ly = parseFloat(prompt("nhap diem ly:"));
+let hoa = parseFloat(prompt("nhap diem hoa:"));
+let sinh = parseFloat(prompt("nhap diem sinh:"));
+
+
+let dtong= ly + hoa + sinh;
+let dtb= dtong/3;
+alert(dtong)
+alert(dtb)
